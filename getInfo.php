63a1dae7793c20c6b64ef721170a565c5006c2a3
Panindra <pani.1990@gmail.com>
2014-12-17 11:39:17 -0600

    addded pie charts

--- getInfo.php
+++ getInfo.php
@@ -1,14 +1,22 @@
 <?php
 
-get_totalTime();
 
-function get_totalTime(){
+//echo $_POST["getWasteTimeInfo"];
+
+if($_POST["getWasteTimeInfo"] == "getWasteTimeInfo") {
+	get_wasteTimeInfo();		
+}
+else {
+	get_totalTimeInfo();	
+}
+
+
+function get_wasteTimeInfo(){
 //$return = intval($_POST["action"]) + 1;
 	$my_file = "week.txt";
 	$handle = fopen($my_file, 'r') or die('Cannot open file:  '.$my_file);
 	
-	$totalStudyTime = 0;
-	$totalStudiedTime = 0;
+	$timeArray = array();
 
 	if ($handle) {
     	while (($line = fgets($handle)) !== false) {
@@ -16,17 +24,83 @@ function get_totalTime(){
     		$value = split(" ", $line);
     		$totalStudyTime += intval($value[1]);
    			$totalStudiedTime += intval($value[2]);
+   			$value[3] = str_replace("\n", "", $value[3]);
+   			if(isset($value[3])) {
+   				$timeArray[$value[3]] = $timeArray[$value[3]] + 1;		
+   			}
+   			else {
+   				$timeArray[$value[3]] = 1;
+   			}
     	}
 	} 
 
 	fclose($handle);
 	
-	$data = $totalStudyTime." ".$totalStudiedTime;
+	$data = $totalStudyTime." ".$totalStudiedTime. " ".$timeArray;
+	echo json_encode($timeArray);
+}
+
+
+function get_totalTimeInfo(){
+	$my_file = "week.txt";
+	$handle = fopen($my_file, 'r') or die('Cannot open file:  '.$my_file);
 	
+	$mondayStudyTime = 0;
+	$mondayStudiedTime = 0;
 
+	$tuesdayStudyTime = 0;
+	$tuesdayStudiedTime = 0;
 
-//$return["json"] = json_encode($return);
-	echo json_encode($data);
+	$wednesdayStudyTime = 0;
+	$wednesdayStudiedTime = 0;
+
+	$thursdayStudyTime = 0;
+	$thursdayStudiedTime = 0;
+
+	$fridayStudyTime = 0;
+	$fridayStudiedTime = 0;
+
+	$patternInfo = array();
+	
+	if ($handle) {
+    	while (($line = fgets($handle)) !== false) {
+        // process the line read.
+    		$value = split(" ", $line);
+
+    		if($value[0] == "Monday") {
+        		$mondayStudyTime += intval($value[1]);
+   				$mondayStudiedTime += intval($value[2]);
+    		}
+    		else if($value[0] == "Tuesday") {
+        		$tuesdayStudyTime += intval($value[1]);
+   				$tuesdayStudiedTime += intval($value[2]);
+    		}
+    		else if($value[0] == "Wednesday") {
+        		$wednesdayStudyTime += intval($value[1]);
+   				$wednesdayStudiedTime += intval($value[2]);
+    		}
+    		else if($value[0] == "Thursday") {
+        		$thursdayStudyTime += intval($value[1]);
+   				$thursdayStudiedTime += intval($value[2]);
+    		}
+    		else if($value[0] == "Friday") {
+        		$fridayStudyTime += intval($value[1]);
+   				$fridayStudiedTime += intval($value[2]);
+    		}
+   			//$patternInfo[$value[0]] = $totalStudyTime, $totalStudiedTime;		
+   		}
+   	$patternInfo["Monday"] = $mondayStudyTime - $mondayStudiedTime;
+   	$patternInfo["Tuesday"] = $tuesdayStudyTime - $tuesdayStudiedTime;
+   	$patternInfo["Wednesday"] = $wednesdayStudyTime - $wednesdayStudiedTime;
+   	$patternInfo["Thursday"] = $thursdayStudyTime - $thursdayStudiedTime;
+   	$patternInfo["Friday"] = $fridayStudyTime - $fridayStudiedTime;
+    }
+
+	fclose($handle);
+	
+	//$data = $totalStudyTime." ".$totalStudiedTime;
+	echo json_encode($patternInfo);
+
 }
 
 ?>
--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 var usreName = ""
 
 $(document).ready(function(){
-  //alert("In document ready");
-//  saveInfo();
-    getInfo();
-
 
    $('#MBtn').click(function() {
        var studyTime = $('#MidealTime').val();
@@ -60,21 +56,54 @@ function saveInfo(studyTime, studiedTime, wastedTimeSource, day) {
       //alert(data1);
     }
   });
-
 }
 
 function getInfo() {
-
   $.ajax({
     url:'/getInfo.php',
-    datatype:"json",
+    dataType:"json",
     type:'POST',
-    data:{},
+    data:{"getWasteTimeInfo":"getWasteTimeInfo"},
     success:function(data1) {
-      //alert(data1);
+      var keys = [];
+      var values = [];
+      var total = 0;
+      $.each(data1, function(k, v){
+        //alert(k);
+        keys.push(k);
+        total += v;
+      });
+
+      $.each(data1, function(k, v){
+        values.push((v/total)*100);
+      });
+     
+     pieChart(keys, values);
     }
   });
 
+  $.ajax({
+    url:'/getInfo.php',
+    dataType:"json",
+    type:'POST',
+    data:{},
+    success:function(data) {
+      var keys = [];
+      var values = [];
+      var total = 0;
+      $.each(data, function(k, v){
+        //alert(k);
+        keys.push(k);
+        total += v;
+      });
+
+      $.each(data, function(k, v){
+        values.push((v/total)*100);
+      });
+      //alert(values);
+     dayspieChart(keys, values);
+    }
+  });
 }
 
 $(document).on('pagebeforeshow', '#grid-page', function(e, data){     
@@ -82,9 +111,12 @@ $(document).on('pagebeforeshow', '#grid-page', function(e, data){
     userName = data.prevPage.find('#test-input').val();
 });
 
+$( document ).on( "pagecreate", "#overview", function() {
+  getInfo();
+}); 
 
 $( document ).on( "pagecreate", "#one", function() {
-    $('#welcomeUser').text("Welcome " + userName + "!");
+   // $('#welcomeUser').text("Welcome " + userName + "!");
     $( document ).on( "swipeleft swiperight", "#one", function( e ) {
 	alert("on swipe");
         if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
@@ -96,7 +128,16 @@ $( document ).on( "pagecreate", "#one", function() {
 });
 
 
-$(function () {
+function pieChart(keys, values) {
+    var seriesData = [];
+    
+     for(var i = 0; i < keys.length; i++) {
+        var data = [];
+        data.push(keys[i]);
+        data.push(values[i]);
+        seriesData.push(data);
+     }
+
     $('#container').highcharts({
         chart: {
             plotBackgroundColor: null,
@@ -125,20 +166,58 @@ $(function () {
         series: [{
             type: 'pie',
             name: 'Browser share',
-            data: [
-                ['Firefox',   45.0],
-                ['IE',       26.8],
-                {
-                    name: 'Chrome',
-                    y: 12.8,
-                    sliced: true,
-                    selected: true
-                },
-                ['Safari',    8.5],
-                ['Opera',     6.2],
-                ['Others',   0.7]
-            ]
+            data: seriesData
         }]
     });
-});
+}
+
+function dayspieChart(keys, values) {
+  var seriesData = [];
+    
+     for(var i = 0; i < keys.length; i++) {
+        var data = [];
+        data.push(keys[i]);
+        data.push(values[i]);
+        seriesData.push(data);
+     }
+
+    $('#container1').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: 'Days<br>Read',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 50
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Days spread',
+            innerSize: '50%',
+            data: seriesData
+        }]
+    });
+}
 
